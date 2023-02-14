6cce8b7daf2b17397de28132681f86877c5239ca
Logan Hankla <chankla@smith.edu>
2023-02-14 11:47:57 -0500

    added imports

--- Conversation.java
+++ Conversation.java
@@ -1,3 +1,13 @@
+/**
+ *Filename: Conversation.java
+*Description: Creating a chatbot for A3 in CSC 120.
+     Author: Logan Hankla (@loganhankla)
+       Date: 15 February 2023
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
 class Conversation {
 
   public static void main(String[] arguments) {
